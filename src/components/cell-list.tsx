8d3b5aaa3d2e261760d91329d22c867a827c2399
Kuro091 <lcminhhp@gmail.com>
2022-05-29 01:05:05 +0700

    adding cumulative code, small fixes

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import "./cell-list.css";
 import React, { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
@@ -16,7 +17,27 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
+      <div
+        style={{
+          margin: "50px 0 0 20px",
+        }}
+      >
+        <p style={{ marginBottom: "5px" }}>
+          <b>Note:</b> There's a div with id of #root for you to mount something
+          like react or vue onto
+        </p>
+        <p style={{ marginBottom: "5px" }}>
+          To display something quickly, you can do: <br />-
+          document.querySelector('#root').innerHtml = 'something' <br />-
+          show('something')
+        </p>
+        <p style={{ marginBottom: "5px" }}>
+          <b>Codes are cumulative</b>. Meaning code cells defined later will
+          have access to the codes written in <b>previous cells</b>
+        </p>
+      </div>
+
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
